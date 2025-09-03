2b38849e71977eb50144cea15ffc1f4f34e6b75b
gabriel <mendesgabrielfernades@gmail.com>
2025-09-03 13:31:08 -0300

    feat: configuration database

--- src/main/java/br/com/altech/rountinner/entity/User.java
+++ src/main/java/br/com/altech/rountinner/entity/User.java
@@ -1,41 +1,30 @@
 package br.com.altech.rountinner.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users") // define o nome da tabela no Postgres
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // ID autoincrement no Postgres
+    private Long id;
+
+    @NotBlank(message = "O nome é obrigatório")
+    @Size(max = 255, message = "O nome deve ter no máximo 255 caracteres")
+    @Column(nullable = false, length = 255)
     private String nome;
+
+    @Size(max = 3, message = "A idade deve ter no máximo 3 caracteres")
+    @Column(length = 3)
     private String idade;
+
+    @NotBlank(message = "O CPF é obrigatório")
+    @Size(max = 14, message = "CPF deve estar no formato XXX.XXX.XXX-XX")
+    @Column(unique = true, length = 14, nullable = false)
     private String cpf;
-
-    public User() {
-    }
-
-    public User(String nome, String idade, String cpf) {
-        this.nome = nome;
-        this.idade = idade;
-        this.cpf = cpf;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getIdade() {
-        return idade;
-    }
-
-    public void setIdade(String idade) {
-        this.idade = idade;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
 }
--- src/main/java/br/com/altech/rountinner/repository/UserRepository.java
+++ src/main/java/br/com/altech/rountinner/repository/UserRepository.java
@@ -1,4 +1,7 @@
 package br.com.altech.rountinner.repository;
 
-public class UserRepository {
+import br.com.altech.rountinner.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
 }
